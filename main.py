73fbfb0e1dcada70e3757842e801bc090b959c43
Bhavesh Daryani <bdaryani@microsoft.com>
2023-06-19 13:13:44 +0530

    added pydantic models schema to post api

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
+from typing import Optional
 from fastapi import FastAPI
 from fastapi.params import Body
+from pydantic import BaseModel
 
 app = FastAPI()
+
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = False
+    rating: Optional[int] = None
 
 
 @app.get("/")
@@ -14,6 +23,7 @@ def get_posts():
     return {"data": "This is your posts"}
 
 @app.post("/createposts")
-def create_posts(payload: dict = Body(...)):
-    print(payload)
-    return {"message": "successfully created post"}
+def create_posts(post: Post):
+    print(post)
+    print(post.dict())
+    return {"new_post": f"title: {post.title} content: {post.content} "}
