7cbc670c3ab4b65a85784f45657dfc26ff0de99a
Bhavesh Daryani <bdaryani@microsoft.com>
2023-06-22 23:01:40 +0530

    resolved some bugs

--- app/oauth2.py
+++ app/oauth2.py
@@ -17,7 +17,7 @@ ACCESS_TOKEN_EXPIRE_TIME = 30
 def create_access_token(data: dict):
     to_encode = data.copy()
 
-    expire = datetime.now() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_TIME)
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_TIME)
     to_encode.update({"exp": expire})
 
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, ALGORITHM)
@@ -26,7 +26,7 @@ def create_access_token(data: dict):
 
 def verify_access_token(token: str, credential_exception):
     try:
-        payload = jwt.decode(token, SECRET_KEY, ALGORITHM)
+        payload = jwt.decode(token, SECRET_KEY, [ALGORITHM])
         id: str = payload.get("user_id")
 
         if id is None:
--- app/routers/auth.py
+++ app/routers/auth.py
@@ -12,10 +12,10 @@ def login(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Session=D
     user = db.query(models.User).filter(models.User.email==user_credentials.username).first()
     
     if not user:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"invalid credentials")
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=f"invalid credentials")
     
     if not utils.verify(user_credentials.password, user.password):
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"invalid credentials")
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=f"invalid credentials")
     
     #create token and return it
     access_token = oauth2.create_access_token(data = {"user_id": user.id})
