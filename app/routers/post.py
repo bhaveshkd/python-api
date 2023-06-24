9abe96cb5ca58f14ca8d9c908ebaa8f88cd2fb0e
Bhavesh Daryani <bdaryani@microsoft.com>
2023-06-24 23:38:14 +0530

    added auth for all post apis and some bug fixed

--- app/routers/post.py
+++ app/routers/post.py
@@ -10,7 +10,7 @@ router = APIRouter(
 )
 
 @router.get("/", response_model=List[schemas.Post])
-def get_posts(db: Session = Depends(get_db)):
+def get_posts(db: Session = Depends(get_db), user_id: int = Depends(oauth2.get_current_user)):
     # cursor.execute("""SELECT * FROM posts""")
     # posts = cursor.fetchall()
     posts = db.query(models.Post).all()
@@ -32,7 +32,7 @@ def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db), user_i
 
 
 @router.get("/{id}", response_model=schemas.Post)
-def get_post(id: int, db: Session = Depends(get_db)):
+def get_post(id: int, db: Session = Depends(get_db), user_id: int = Depends(oauth2.get_current_user)):
     # cursor.execute("""SELECT * FROM posts WHERE id = %s""", (str(id)))
     # post = cursor.fetchone();
 
@@ -45,7 +45,7 @@ def get_post(id: int, db: Session = Depends(get_db)):
 
 
 @router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(id: int, db: Session = Depends(get_db)):
+def delete_post(id: int, db: Session = Depends(get_db), user_id: int = Depends(oauth2.get_current_user)):
     # cursor.execute("""DELETE FROM posts WHERE ID = %s RETURNING * """,(str(id)))
     # deleted_post = cursor.fetchone()
     # conn.commit()
@@ -62,7 +62,7 @@ def delete_post(id: int, db: Session = Depends(get_db)):
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 @router.put("/{id}", response_model=schemas.Post)
-def update_post(id: int, updated_post: schemas.PostCreate, db: Session = Depends(get_db)):
+def update_post(id: int, updated_post: schemas.PostCreate, db: Session = Depends(get_db), user_id: int = Depends(oauth2.get_current_user)):
     # cursor.execute("""UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING * """, (post.title, post.content, post.published, str(id)))
 
     # updated_post = cursor.fetchone()
--- app/schemas.py
+++ app/schemas.py
@@ -35,7 +35,7 @@ class UserLogin(BaseModel):
     password: str
 
 class Token(BaseModel):
-    access_token: EmailStr
+    access_token: str
     token_type: str
 
 class TokenData(BaseModel):
