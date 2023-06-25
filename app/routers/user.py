6587a3f6916c3ec1fe3919578ccee7f6efb3a07a
Bhavesh Daryani <bdaryani@microsoft.com>
2023-06-26 00:16:35 +0530

    added get all users api

--- app/routers/user.py
+++ app/routers/user.py
@@ -2,18 +2,26 @@ from fastapi import FastAPI, Depends, status, HTTPException, APIRouter
 from .. import models, schemas, utils
 from sqlalchemy.orm import Session
 from ..database import get_db
+from typing import List
 
 router = APIRouter(
     prefix="/users",
     tags=['Users']
 )
 
+@router.get("/", response_model=List[schemas.UserOut])
+def get_users(db: Session = Depends(get_db)):
+    users = db.query(models.User).all()
+    if not users:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail = f"user with id: {id} does not exist!")
+
+    return users
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.UserOut)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     # hashing the user password
     hashed_password = utils.hash(user.password)
-    user.password = hashed_password    
+    user.password = hashed_password     
     new_user = models.User(**user.dict())
     db.add(new_user)
     db.commit()
